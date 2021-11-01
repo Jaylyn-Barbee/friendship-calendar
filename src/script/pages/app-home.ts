e58ef5de14858db17e32728ec5e72aa1630a5276
Jaylyn-Barbee <jaylyn.1b@gmail.com>
2021-11-01 12:40:40 -0400

    login successful and calling the calendar api successful

--- src/script/pages/app-home.ts
+++ src/script/pages/app-home.ts
@@ -13,57 +13,6 @@ export class AppHome extends LitElement {
 
   static get styles() {
     return css`
-      #welcomeBar {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-      }
-
-      #welcomeBar fast-card {
-        margin-bottom: 12px;
-      }
-
-      #welcomeCard,
-      #infoCard {
-        padding: 18px;
-        padding-top: 0px;
-      }
-
-      pwa-install {
-        position: absolute;
-        bottom: 16px;
-        right: 16px;
-      }
-
-      button {
-        cursor: pointer;
-      }
-
-      @media (min-width: 1200px) {
-        #welcomeCard,
-        #infoCard {
-          width: 40%;
-        }
-      }
-
-      @media (screen-spanning: single-fold-vertical) {
-        #welcomeBar {
-          flex-direction: row;
-          align-items: flex-start;
-          justify-content: space-between;
-        }
-
-        #welcomeCard {
-          margin-right: 64px;
-        }
-      }
-
-      @media(prefers-color-scheme: light) {
-        fast-card {
-          --background-color: white;
-        }
-      }
     `;
   }
 
@@ -74,22 +23,12 @@ export class AppHome extends LitElement {
   async firstUpdated() {
     // this method is a lifecycle even in lit
     // for more info check out the lit docs https://lit.dev/docs/components/lifecycle/
-
   }
 
-  share() {
-    if ((navigator as any).share) {
-      (navigator as any).share({
-        title: 'PWABuilder pwa-starter',
-        text: 'Check out the PWABuilder pwa-starter!',
-        url: 'https://github.com/pwa-builder/pwa-starter',
-      });
-    }
-  }
 
   render() {
     return html`
-
+    <button @click="${() => test()}"> test </button>
     `;
   }
 }
