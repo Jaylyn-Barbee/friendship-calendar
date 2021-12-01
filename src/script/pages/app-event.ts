2df51980d1a159f3526556822a56d19f84322557
Jaylyn Barbee <jaylyn.1b@gmail.com>
2021-12-01 15:33:17 -0500

    starting to work on event form

--- src/script/pages/app-event.ts
+++ src/script/pages/app-event.ts
@@ -8,6 +8,94 @@ export class AppEvent extends LitElement {
 
   static get styles() {
     return css`
+    #page {
+        background-color: #F1E4EE;
+        height: 100vh;
+    }
+
+    section {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        min-height: 400px;
+        padding-top: 100px;
+        background: #DDBDD5;
+    }
+
+    .curve {
+        position: absolute;
+        height: 250px;
+        width: 100%;
+        bottom: 0;
+        text-align: center;
+    }
+
+    .curve::before {
+        content: '';
+        display: block;
+        position: absolute;
+        border-radius: 100% 50%;
+        width: 55%;
+        height: 100%;
+        transform: translate(85%, 60%);
+        background-color: #F1E4EE;
+        z-index: 1;
+    }
+
+    .curve::after {
+        content: '';
+        display: block;
+        position: absolute;
+        border-radius: 100% 50%;
+        width: 55%;
+        height: 100%;
+        background-color: #DDBDD5;
+        transform: translate(-4%, 40%);
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+
+        height: fit-content;
+        width: 35vw;
+        background-color: white;
+
+        border-radius: 10px;
+        padding: 20px;
+
+        position: absolute;
+        z-index: 100;
+        top: 50%;  /* position the top  edge of the element at the middle of the parent */
+        left: 50%; /* position the left edge of the element at the middle of the parent */
+
+        transform: translate(-50%, -50%);
+    }
+
+    form input {
+        margin-bottom: 10px;
+    }
+
+    #submit {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        font-size: 16px;
+        font-weight: bolder;
+        padding: 10px;
+        margin-top: 10px;
+
+        border-radius: 5px;
+        background-color: #F1E4EE;
+        border: none;
+      }
+
+      #submit:hover {
+          cursor: pointer;
+          background-color: #ddbdd5
+      }
 
     `;
   }
@@ -27,7 +115,10 @@ export class AppEvent extends LitElement {
   render() {
     return html`
         <div id="page">
-            <form id="create_event_form" @submit=${(event: any) => this.handleSubmit(event)}>
+            <section>
+                <div class="curve"></div>
+            </section>
+            <form id="create_event_form">
                 <label for="event_name">Event Name:</label>
                 <input type="text" id="event_name" name="event_name">
 
@@ -43,9 +134,10 @@ export class AppEvent extends LitElement {
                 <label for="event_location">Event Location:</label>
                 <input type="text" id="event_location" name="event_location">
 
-                <label for="event_attendees">Event Location:</label>
+                <label for="event_attendees">Attendees:</label>
                 <mgt-people-picker></mgt-people-picker>
-                <!-- Put the person cards for those selected. -->
+
+                <input id="submit" type="button" value="Add New Event"  @click=${(event: any) => this.handleSubmit(event)}/>
             </form>
         </div>
     `;
